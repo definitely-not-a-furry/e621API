881fc1bcb9981896ef47654a5c5edeec03d2cf83
definitely-no-a-furry <HeijnisTies@outlook.com>
2023-02-10 19:23:09 +0100

    small QOL changes

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
-import os, requests, json
+import os
+import json
+import requests
 
-configfile = open('config.json',) # load configuration file
+configfile = open('config.json',encoding='UTF-8') # load configuration file
 config=json.load(configfile)
 silent_mode = config['silent-mode']
 debug_mode = config['debug-mode']
@@ -8,9 +10,9 @@ debug_mode = config['debug-mode']
 filenames = []
 
 #url = "https://static1.e621.net/data/af/30/af30659e164e085c745814bf1174cafb.gif" # temporary testing link
-urls = open('tmp\\links').read().split('\n')
+urls = open('tmp\\links',encoding='UTF-8').read().split('\n')
 
-if urls == '':
+if urls is '':
     print('No posts found')
     os.system('pause')
 
@@ -20,7 +22,7 @@ def removeprefix(item):
     filename = filename[1:]
     filename = filename[filename.find('/'):]
     filename = filename[1:]
-    return(filename)
+    return filename
 
 if not os.path.exists('out'): # create folder if it does not exist
     os.makedirs('out')
@@ -29,12 +31,12 @@ for i in urls:
     file_path=(os.path.join('out',removeprefix(i)))
     try:
         r = requests.get(i, stream=True)
-    except:
-        raise Exception('Exeption: was unable to access file. Check if you can access the website using a browser.')
+    except Exception:
+        None
     if r.ok:
-        if silent_mode == False and debug_mode == False:
+        if silent_mode is False and debug_mode is False:
             print(i)
-        elif debug_mode == True:
+        elif debug_mode is True:
             print(f'Downloading "{i}" to "{file_path}"')
         with open(file_path, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024 * 8):
@@ -43,4 +45,4 @@ for i in urls:
                     f.flush()
                     os.fsync(f.fileno())
     else:  # HTTP status code 4XX/5XX
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
+        print(f"Download failed: status code {r.status}\n{r.text}")
